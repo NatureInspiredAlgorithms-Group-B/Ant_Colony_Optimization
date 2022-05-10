536c1fe8b15b05ec11ece390c3fbf1fc9247cd1d
Johanna Linkemeyer <jlinkemeyer@uni-osnabrueck.de>
2022-05-10 17:59:24 +0200

    update comments in aco.py

--- aco.py
+++ aco.py
@@ -1,16 +1,17 @@
 from itertools import count
 import random
+from graph import *
 
 
 class Ant:
     def __init__(self, graph, neighbors, valid, alpha=1, beta=1):
         """
-
+        An ant is supposed to travel within a graph, visiting all nodes exactly once.
         :param graph: graph in which the ant is traveling
-        :param neighbors: the nodes the ant can travel to next
-        :param valid:
-        :param alpha:
-        :param beta:
+        :param neighbors: function that returns the neighbours of the node the ant currently is on
+        :param valid: True once an ant has finished its solution
+        :param alpha: importance of pheromone quantity
+        :param beta: importance of heuristic information
         """
         self.graph = graph
         self.neighbors = lambda n: neighbors(self, n)
@@ -21,9 +22,9 @@ class Ant:
 
     def __contains__(self, other):
         """
-        Tests, whether a given ... is contained in either the nodes or edges within the current path.
-        :param other:
-        :return:
+        Allows to test, whether a given ... is contained in either the nodes or edges within the current path.
+        :param other: # TODO
+        :return: True if ... already is in the path (either as a node or edge), False otherwise
         """
         if isinstance(other, Node):
             return other in self.path_nodes
@@ -34,20 +35,20 @@ class Ant:
 
     def reset(self):
         """
-        Resets the current 'status' of an ant, i.e. ant is initialized in a random node within the graph and has not
+        Resets the current 'status' of an ant, i.e. ant is initialized on a random node within the graph and has not
         travelled yet.
-        :return:
+        :return: ---
         """
-        self.length = 0 # distance travelled
-        self.node = random.choice(self.graph) # current node on which the ant resides
-        self.path_nodes = [self.node] # current path of ant described through nodes
-        self.path_edges = [] # current path of the ant described through edges
+        self.dist_travelled = 0  # distance travelled
+        self.node = random.choice(self.graph)  # current node on which the ant resides
+        self.path_nodes = [self.node]  # current path of ant described through nodes
+        self.path_edges = []  # current path of the ant described through edges
 
     def step(self):
         """
         Performs the next step of the ant, choosing the next node based on transition probabilities and adds the node
         and edges to the current path information.
-        :return:
+        :return: True if a next step was performed, False otherwise
         """
         # Get the transition options and probabilities
         nodes, probabilities = self.transitions()
@@ -62,8 +63,8 @@ class Ant:
         edge = self.graph[self.node, node]
         self.path_nodes.append(node)
         self.path_edges.append(edge)
-        self.length += edge.value # Update the travelled distance
-        self.node = node # Update the current node
+        self.dist_travelled += edge.value  # Update the total travelled distance of this ant
+        self.node = node  # Update the current node
 
         return True
 
@@ -86,7 +87,7 @@ class Ant:
         # Perform the calculations to receive the probabilities to determine the next node
         eta = np.array(heuristic)
         tau = np.array(pheromone)
-        rho = tau ** self.alpha * eta ** self.beta
+        rho = tau ** self.alpha * eta ** self.beta  # TODO this is not the same rho as in the AntColony, right? Please rename this according to your preference
         return nodes, rho/np.sum(rho)
 
 
@@ -96,21 +97,22 @@ class AntColony:
                                      or len(a.graph) == len(a.path_nodes) and n == a.path_edges[0].source,
                  valid=lambda a: len(a.path_nodes) == len(a.graph) + 1):
         """
-        An ant colony consists of a given number of ants, a graph in which the ants travel and ...
-        :param n_ants:
-        :param graph:
-        :param rho:
-        :param alpha:
-        :param beta:
-        :param neighbors:
-        :param valid:
+        An ant colony consists of a given number of ants, a graph in which the ants travel and parameter information on
+        the algorithm: rho, alpha, and beta.
+        :param n_ants: Number of ants within the colony
+        :param graph: Graph with a problem to solve
+        :param rho: Evaporation rate
+        :param alpha: importance of pheromone quantity
+        :param beta: importance of heuristic information
+        :param neighbors: a function how the neighbours of a node are defined
+        :param valid: a function that determines if ants have found a valid solution
         """
         self.n_ants = n_ants
         self.graph = graph
         self.rho = rho
         self.alpha = alpha
         self.beta = beta
-        self.graph.edges.heuristic = 1/self.graph.edges.value
+        self.graph.edges.heuristic = 1 / (self.graph.edges.value + 0.00001)  # TODO avoid potential division by zero
         self.ants = [Ant(self.graph, neighbors, valid, alpha, beta) for _ in range(n_ants)]
         self.node = random.choice(graph)
         self.graph.edges.pheromone = 1.0
@@ -118,16 +120,18 @@ class AntColony:
     def __call__(self, N=None, visualization=False):
         """
         Executes the ant colony optimization algorithm.
-        :param N:
-        :param visualization:
-        :return:
+        :param N: number of iterations
+        :param visualization: whether intermediate results should be visualized
+        :return: # TODO
         """
         iterator = count() if N is None else range(N)
         self.min_length = float('inf')
         self.min_path = None
         for _ in iterator:
-            for ant in self.ants:
+            # Reset all ants at the beginning of each iteration
+            for ant in self:
                 ant.reset()
+            # Let ants create their solution and update pheromones based on those
             self.construct(visualization=visualization)
             self.daemon()
             self.update()
@@ -135,56 +139,45 @@ class AntColony:
 
     def __iter__(self):
         """
-
-        :return:
+        Allows to iterate over the ants in the colony
+        :return: ---
         """
         for ant in self.ants:
             yield ant
 
-
     def construct(self, visualization=False):
         """
-        Constructs ant solutions
-        :param visualization:
-        :return:
+        Constructs ant solutions as long as all ants can take a further step.
+        :param visualization: whether intermediate results should be visualized
+        :return: ---
         """
         while any(ant.step() for ant in self):
             if visualization:
                 self.visualize()
 
-
     def daemon(self):
-        """(Optionally) Daemon Actions"""
+        """
+        (Optionally) Daemon actions # TODO @Mortimer please update this as well, I am unsure about well-defined comments
+        :return: ---
+        """
         min_path, min_length = min(((ant.path_nodes, ant.length)
             for ant in self if ant.valid()), key=lambda t: t[1])
-        min_ant = min(self, key=lambda t: t.length)
+        min_ant = min(self, key=lambda t: t.length) # TODO min_ant is not used
         if self.min_length > min_length:
             self.min_length = min_length
             self.min_path = list(min_path)
 
-
     def update(self, F=lambda x: 1/x):
         """
-        Updates pheromone trails
-        :param F:
-        :return:
+        Updates pheromone trails after all ants have finished their travels during one iteration.
+        :param F: function to create the multiplicative inverse of a given number
+        :return: ---
         """
+        # Get all paths that all ants have taken
         ants = [ant for ant in self if ant.valid()]
-        paths = [ant.path_edges for ant in self if ant.valid()]
+
+        # Update the pheromones on all edges based on the previous pheromones weighted through the evaporation rate, and
+        # the newly added pheromones
         for edge in set(edge for ant in ants for edge in ant.path_edges):
             S = sum(F(ant.length) for ant in ants if edge in ant)
             edge.pheromone = (1 - self.rho) * edge.pheromone + self.rho * S
-
-
-if __name__ == '__main__':
-    from graph import *
-
-    G = TSP(6)
-    C = AntColony(10, G, alpha=1, beta=10, rho=0.1)
-    search_path, search_length = G.route()
-    ant_path, ant_length = C(100)
-    print(G.edges.value)
-    print(ant_path, search_path)
-    print(ant_length, search_length)
-
-
--- main.py
+++ main.py
@@ -1,74 +1,15 @@
-from graph import Graph
-from aco import AntColonyOpt as ACO
-import random
-import numpy as np
-import matplotlib.pyplot as plt
+from graph import *
+from aco import AntColony
 
-if __name__ == "__main__":
 
-    # define algorithm parameters
-    num_ants = 20
-    evaporation_rate = 0.05
-    initial_pheromone = 0.01
-    num_iterations = 100
-    alpha = 0.6
-    beta = 0.8
+if __name__ == '__main__':
 
-    characters = "abcdefghijklmnopqrstuvwxyz123456789"
-
-    grid = np.array([char for char in characters]).reshape((5, 7))
-
-    # Initialize the test_dict with the connections within a grid
-    test_dict = {}
-    for i in range(grid.shape[0]):
-        for j in range(grid.shape[1]):
-
-            node = grid[i, j]
-            test_dict[node] = []
-
-            # 'connect' current node with the left, right, up, down neighbours
-            if i >= 1:
-                test_dict[node].append(grid[i-1, j])
-            if j >= 1:
-                test_dict[node].append(grid[i, j-1])
-            if i < grid.shape[0]-1:
-                test_dict[node].append(grid[i+1, j])
-            if j < grid.shape[1]-1:
-                test_dict[node].append(grid[i, j+1])
-
-    start = "3"
-    goal = "g"
-    test_graph = Graph(test_dict, goal=goal, pheromone_value=initial_pheromone, grid=grid)
-    test_ACO = ACO(test_graph, evaporation_rate, alpha, beta)
-
-    # start ant colony optimization
-    for i in range(0, num_iterations):
-
-        print("Iteration ", i)
-        test_ACO.tmp = Graph(test_dict)
-        #foodsources = ["c", "l", "z", "5"]
-
-        # in each iteration, each ant is supposed to find the food source starting from a random position
-        for _ in range(0, num_ants):
-            start = random.choice(test_graph.get_nodes())
-            test_ACO.create_path(start=start, foodsources=[], goal=goal)
-        test_ACO.pheromone_update()
-
-        # img = np.zeros(grid.shape).astype(np.float64)
-        # for (i, j) in test_ACO.graph.get_pheromones().keys():
-        #     img[grid == j] += test_ACO.graph.get_pheromones()[(i, j)]
-        #
-        # plt.imshow(img, cmap='gray')
-        # plt.show()
-
-    final_pheromones = test_ACO.graph.get_pheromones()
-
-    img = np.zeros(grid.shape).astype(np.float64)
-    for (i, j) in final_pheromones.keys():
-        img[grid == j] += final_pheromones[(i, j)]
-
-    plt.imshow(img, cmap='gray')
-    plt.show()
-
+    G = TSP(6)
+    C = AntColony(10, G, alpha=1, beta=10, rho=0.1)
+    search_path, search_length = G.route()
+    ant_path, ant_length = C(100)
+    print(G.edges.value)
+    print(ant_path, search_path)
+    print(ant_length, search_length)
 
 
